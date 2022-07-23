93e203a3fab0f2cc13c81e513728d1a736d261bb
5HT <maxim@synrc.com>
2022-07-23 04:40:40 +0300

    /c/

--- src/world_loader.erl
+++ src/world_loader.erl
@@ -2,7 +2,7 @@
 -export ([boot/0]).
 
 boot() ->
-  case kvs:get(writer, <<"/countries/Україна"/utf8>>) of
+  case kvs:get(writer, <<"/c/Україна"/utf8>>) of
     {ok,_} -> skip;
     {error,_} ->
        world:warning("World import was done.", [])
